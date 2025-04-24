28db1055078ad6c02b3ed4c652c9a826ff2acf73
Mylon Jones <mylonndj1234@gmail.com>
2025-04-23 21:08:19 -0400

    comments in fine tune model

--- docker_settings/fine-tune-model.py
+++ docker_settings/fine-tune-model.py
@@ -27,17 +27,11 @@ from transformers import (
     TrainingArguments,
     set_seed,
 )
-from transformers.trainer_utils import get_last_checkpoint
-from transformers.utils import check_min_version
-from transformers.utils.versions import require_version
+# from transformers.trainer_utils import get_last_checkpoint
+# from transformers.utils import check_min_version
 
 
-""" Fine-tuning a 🤗 Transformers model for image classification"""
-
-# Will error if the minimal version of Transformers is not installed. Remove at your own risks.
-check_min_version("4.52.0.dev0")
-
-require_version("datasets>=2.14.0", "To fix: pip install -r examples/pytorch/image-classification/requirements.txt")
+# check_min_version("4.52.0.dev0")
 
 MODEL_CONFIG_CLASSES = list(MODEL_FOR_IMAGE_CLASSIFICATION_MAPPING.keys())
 MODEL_TYPES = tuple(conf.model_type for conf in MODEL_CONFIG_CLASSES)
@@ -68,9 +62,9 @@ def main():
     trainer_args.run_name = "./model/"
 
     # Detecting last checkpoint.
-    last_checkpoint = None
-    last_checkpoint = get_last_checkpoint(output_dir)
-    if last_checkpoint is None and len(os.listdir(output_dir)) > 0: exit()
+    # last_checkpoint = None
+    # last_checkpoint = get_last_checkpoint(output_dir)
+    # if last_checkpoint is None and len(os.listdir(output_dir)) > 0: exit()
 
     # Set seed before initializing model.
     set_seed(seed)
@@ -88,12 +82,15 @@ def main():
         cache_dir=None,
     )
 
+
+    # What does this do?
     def collate_fn(examples):
         pixel_values = torch.stack([example["pixel_values"] for example in examples])
         labels = torch.tensor([example["label"] for example in examples])
         return {"pixel_values": pixel_values, "labels": labels}
 
     
+    # split training and validation data
     split = dataset["train"].train_test_split(train_val_split)
     dataset["train"] = split["train"]
     dataset["validation"] = split["test"]
@@ -109,12 +106,12 @@ def main():
     # Load the accuracy metric from the datasets package
     metric = evaluate.load("accuracy", cache_dir=None)
 
-    # Define our compute_metrics function. It takes an `EvalPrediction` object (a namedtuple with a
-    # predictions and label_ids field) and has to return a dictionary string to float.
+    # What's going on here
     def compute_metrics(p):
         """Computes accuracy on a batch of predictions"""
         return metric.compute(predictions=np.argmax(p.predictions, axis=1), references=p.label_ids)
 
+    # how do these work? do we need all these arguments?
     config = AutoConfig.from_pretrained(
         model_name_or_path,
         num_labels=len(labels),
@@ -144,6 +141,7 @@ def main():
         trust_remote_code=False,
     )
 
+    # I have no idea what's going on here
     # Define torchvision transforms to be applied to each image.
     if isinstance(image_processor, TimmWrapperImageProcessor):
         _train_transforms = image_processor.train_transforms
@@ -176,6 +174,8 @@ def main():
             ]
         )
 
+
+    # functions used in trining
     def train_transforms(example_batch):
         """Apply _train_transforms across a batch."""
         example_batch["pixel_values"] = [
@@ -206,8 +206,8 @@ def main():
 
     # Training
     checkpoint = None
-    if last_checkpoint is not None:
-        checkpoint = last_checkpoint
+    # if last_checkpoint is not None:
+    #     checkpoint = last_checkpoint
     train_result = trainer.train(resume_from_checkpoint=checkpoint)
     trainer.save_model()
     trainer.log_metrics("train", train_result.metrics)
@@ -219,6 +219,7 @@ def main():
     trainer.log_metrics("eval", metrics)
     trainer.save_metrics("eval", metrics)
 
+    # I don't think we need this
     # Write model card and (optionally) push to hub
     kwargs = {
         "finetuned_from": model_name_or_path,
